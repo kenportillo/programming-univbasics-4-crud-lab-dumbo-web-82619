c3ce3d8399ee77c50bc43f956acd9f2070272dc5
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-19 21:38:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,6 @@ def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[2]
   array.index["am"]
-  
 end
 
 def retrieve_first_element_from_array(array)
